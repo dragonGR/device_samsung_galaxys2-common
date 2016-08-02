7a3b2c8cc462139563f5e2c2bba6f1e4576fbd4b
rINanDO <drjisakh@gmail.com>
2016-08-02 14:46:52 +0200

    galaxys2-common: Introduce SamsungDoze (1/2)
    Change-Id: I52ee1692423e1e4ed07e2dc53754c18e5dc75e69

--- common.mk
+++ common.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# SamsungDoze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
