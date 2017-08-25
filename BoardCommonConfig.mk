f231eb24cb367da62c33258f2ccc75eb4903f73a
Alex Tsanis <alextsanisbadlorg@gmail.com>
2017-08-25 07:51:22 +0000

    galaxys2-common: Get rid of recovery folder
    Change-Id: I6d4f57f7af8f1fb71f3c36346675e0b0d5b1c28a
    Signed-off-by: Alex Tsanis <alextsanisbadlorg@gmail.com>

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -141,8 +141,6 @@ BOARD_CUSTOM_BT_CONFIG := device/samsung/galaxys2-common/bluetooth/vnd_smdk4210.
 BOARD_SEPOLICY_DIRS += device/samsung/galaxys2-common/selinux
 
 # Recovery
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxys2-common/recovery/recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/galaxys2-common/recovery/graphics.c
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
